688cef1730d61c395cbbe253d6ccaeb253e2d75d
gpspelle <gpsunicamp016@gmail.com>
2022-01-13 15:58:49 -0300

    check for price value when put, patch product

---
+++ cdk.out/asset.0a3ce60b5ce91899bd850575a70668d1b01943261095631d0759d5297475b11f/index.js
@@ -0,0 +1,232 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk")
+const { v4: uuidv4 } = require("uuid")
+const { 
+  REGION, 
+  PRODUCTS_TABLE,
+  PRODUCTS_TABLE_PARTITION_KEY,
+  PRODUCT_TAGS_TABLE,
+  PRODUCT_TAGS_TABLE_PARTITION_KEY,
+  IMAGES_BUCKET,
+  NO_TAGS_STRING
+} = process.env;
+// Set the region
+AWS.config.update({ region: REGION })
+
+// Create the DynamoDB service object
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
+var docClient = new AWS.DynamoDB.DocumentClient();
+const S3Client = new AWS.S3()
+
+/*!
+ * node-s3-url-encode - Because s3 urls are annoying
+ */
+
+var encodings = {
+  '\+': "abc",
+  '\!': "bcd",
+  '\"': "cde",
+  '\#': "def",
+  '\$': "efg",
+  '\&': "fgh",
+  '\'': "ghi",
+  '\(': "hij",
+  '\)': "ijk",
+  '\*': "jkl",
+  '\,': "klm",
+  '\:': "lmn",
+  '\;': "mno",
+  '\=': "nop",
+  '\?': "opq",
+  '\@': "pqr",
+};
+
+function encodeS3URI(filename) {
+  return encodeURI(filename) // Do the standard url encoding
+              .replace(
+                  /(\+|!|"|#|\$|&|'|\(|\)|\*|\+|,|:|;|=|\?|@)/img,
+                  function(match) { return encodings[match]; }
+              );
+
+}
+
+const handleError = (error) => {
+  console.error(error);
+
+  return {
+    statusCode: error.statusCode,
+    body: JSON.stringify({ message: error.message }),
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      'Content-Type': 'application/json'
+    },
+    isBase64Encoded: false
+  };
+}
+
+const dealPriceIsHigherThanPrice = "O preço promocional deve ser menor do que o preço padrão do produto"
+
+const main = async (event) => {
+  const task = JSON.parse(event.body)
+  const name = task.name
+  const description = task.description
+  const price = task.price
+  const tags = task.tags
+  const images = task.images
+  const coverImage = task.coverImage
+  const productType = task.productType
+  const productOwnerId = event.requestContext.authorizer.id
+  const id = uuidv4()
+
+  const productImages = images.map((image) => ({
+      S: `https://${IMAGES_BUCKET}.s3.${REGION}.amazonaws.com/${id}/${encodeS3URI(image.name.replace(/ /g, ""))}`
+  }))
+
+  const productImagesResized = images.map((image) => ({
+    S: `https://${IMAGES_BUCKET}.s3.${REGION}.amazonaws.com/${id}/resized-${encodeS3URI(image.name.replace(/ /g, ""))}`
+  }))
+
+  const dynamodbParams = {
+    TableName: PRODUCTS_TABLE,
+    Item: {
+      [PRODUCTS_TABLE_PARTITION_KEY]: { S: id },
+      PRODUCT_OWNER_ID: { S: productOwnerId },
+      PRODUCT_NAME: { S: name },
+      PRODUCT_DESCRIPTION: { S: description },
+      PRODUCT_PRICE: { N: price },
+      PRODUCT_IMAGES: { L: productImages },
+      PRODUCT_IMAGES_RESIZED: { L: productImagesResized },
+      PRODUCT_COVER_IMAGE: { S: coverImage },
+      PRODUCT_TAGS: { SS: tags.length > 0 ? tags : [NO_TAGS_STRING] },
+      PRODUCT_TYPE: { S: productType },
+    },
+  }
+
+  if (productType === "DEAL") {
+    const dealPrice = task.dealPrice
+    dynamodbParams.Item.DEAL_PRICE = { N: dealPrice }
+
+    if (dealPrice >= price) {
+      return handleError({ message: dealPriceIsHigherThanPrice, statusCode: 500 })
+    }
+  } else if (productType === "LIGHTING_DEAL") {
+    const lightingDealStartTime = task.lightingDealStartTime
+    const lightingDealDuration = task.lightingDealDuration
+    const lightingDealEndTime = task.lightingDealEndTime
+    const dealPrice = task.dealPrice
+
+    if (dealPrice >= price) {
+      return handleError({ message: dealPriceIsHigherThanPrice, statusCode: 500 })
+    }
+
+    dynamodbParams.Item.LIGHTING_DEAL_START_TIME = { S: lightingDealStartTime }
+    dynamodbParams.Item.LIGHTING_DEAL_END_TIME = { S: lightingDealEndTime }
+    dynamodbParams.Item.LIGHTING_DEAL_DURATION = { S: lightingDealDuration }
+    dynamodbParams.Item.DEAL_PRICE = { N: dealPrice }
+  }
+
+  // Call DynamoDB to add the item to the table
+  try {
+    await ddb.putItem(dynamodbParams).promise()
+  } catch (error) {
+    console.error(error)
+    return handleError({ message: "Erro ao inserir item no dynamodb", statusCode: error.statusCode })
+  }
+
+  var promises = [];
+  tags.forEach((tag) => {
+    const updateTagsParams = {
+      TableName: PRODUCT_TAGS_TABLE,
+      Key: {
+        [PRODUCT_TAGS_TABLE_PARTITION_KEY]: tag
+      },
+      UpdateExpression: "ADD #p :start",
+      ExpressionAttributeNames : {
+        "#p" : "products"
+      },
+      ExpressionAttributeValues: {
+        ":start": docClient.createSet([id]),
+      },
+      ReturnValues: "UPDATED_NEW",
+    }
+
+    const promise = docClient.update(updateTagsParams).promise();
+    promises.push(promise)
+  })
+
+  try {
+    await Promise.all(promises)
+    console.log("A tag dos produtos foram adicionadas na tabela de tags")
+  } catch (error) {
+    console.error(error)
+    return handleError({ message: "Erro ao adicionar produto na tabela de tags", statusCode: 400 })
+  }
+
+  promises = [];
+
+  images.forEach((image) => {
+    const S3EncodedKey = `${id}/${encodeS3URI(image.name.replace(/ /g, ""))}`
+    const base64Data = new Buffer.from(image.content.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+    const type = image.content.split(';')[0].split('/')[1];
+  
+    const S3Params = {
+      Bucket: IMAGES_BUCKET,
+      Key: S3EncodedKey, // File name you want to save as in S3
+      Body: base64Data,
+      ContentEncoding: 'base64', // required
+      ContentType: `image/${type}` // required. Notice the back ticks
+    } 
+  
+    // Uploading files to the bucket
+    const promise = S3Client.upload(S3Params).promise();
+    promises.push(promise);
+  })
+
+  try {
+    await Promise.all(promises);
+    console.log("Imagens adicionadas ao s3 bucket com sucesso")
+  } catch (error) {
+    console.error(error)
+    return handleError({ message: "Erro ao adicionar imagem no s3 bucket", statusCode: 400 })
+  }
+
+  promises = [];
+
+  images.forEach((image) => {
+    const S3EncodedKey = `${id}/resized-${encodeS3URI(image.name.replace(/ /g, ""))}`
+    const base64Data = new Buffer.from(image.contentResized.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+    const type = image.contentResized.split(';')[0].split('/')[1];
+  
+    const S3Params = {
+      Bucket: IMAGES_BUCKET,
+      Key: S3EncodedKey, // File name you want to save as in S3
+      Body: base64Data,
+      ContentEncoding: 'base64', // required
+      ContentType: `image/${type}` // required. Notice the back ticks
+    } 
+  
+    // Uploading files to the bucket
+    const promise = S3Client.upload(S3Params).promise();
+    promises.push(promise);
+  })
+
+  try {
+    await Promise.all(promises)
+    console.log("Imagens redimensionadas adicionadas ao s3 bucket com sucesso")
+  } catch (error) {
+    console.error(error)
+    return handleError({ message: "Erro ao adicionar imagem redimensionada no s3 bucket", statusCode: 400 })
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Success"),
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Content-Type": "application/json"
+    },
+    isBase64Encoded: false
+  };
+}
+
+module.exports = { main }
---
+++ cdk.out/asset.de13bac98eee5b06950ccc6be90a3a750a66339a5511757670af7d6bd16d91d1/index.js
@@ -0,0 +1,373 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk")
+const { 
+  REGION, 
+  PRODUCTS_TABLE,
+  PRODUCTS_TABLE_PARTITION_KEY,
+  PRODUCT_TAGS_TABLE,
+  PRODUCT_TAGS_TABLE_PARTITION_KEY,
+  IMAGES_BUCKET,
+  NO_TAGS_STRING,
+} = process.env;
+// Set the region
+AWS.config.update({ region: REGION })
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const S3Client = new AWS.S3()
+
+const handleError = (error) => {
+  console.error(error);
+
+  return {
+    statusCode: error.statusCode,
+    body: JSON.stringify({ message: error.message }),
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Content-Type": "application/json"
+    },
+    isBase64Encoded: false
+  };
+}
+
+const removeProductFromTag = async (oldTag, id) => {
+  const params = {
+    TableName: PRODUCT_TAGS_TABLE,
+    Key: { [PRODUCT_TAGS_TABLE_PARTITION_KEY]: oldTag },
+    ExpressionAttributeNames: {
+      "#p": "products"
+    },
+    ExpressionAttributeValues: {
+      ":id": docClient.createSet([id]),
+    },
+    UpdateExpression: "DELETE #p :id",
+  }
+
+  await docClient.update(params).promise()
+}
+
+const addProductToTag = async (tag, id) => {
+  const params = {
+    TableName: PRODUCT_TAGS_TABLE,
+    Key: { [PRODUCT_TAGS_TABLE_PARTITION_KEY]: tag },
+    ExpressionAttributeNames: {
+      "#p": "products"
+    },
+    ExpressionAttributeValues: {
+      ":id": docClient.createSet([id]),
+    },
+    UpdateExpression: "ADD #p :id",
+  }
+
+  await docClient.update(params).promise()
+}
+
+
+const getItemFromDynamoDB = async (id) => {
+  const params = {
+    TableName: PRODUCTS_TABLE,
+    Key: { [PRODUCTS_TABLE_PARTITION_KEY]: id }
+  }
+
+  return docClient.get(params).promise();
+}
+
+const updateItemOnDynamoDB = async (item, idAttributeName) => {
+  const params = {
+      TableName: PRODUCTS_TABLE,
+      Key: {},
+      ExpressionAttributeValues: {},
+      ExpressionAttributeNames: {},
+      UpdateExpression: "",
+  };
+
+  params["Key"][idAttributeName] = item[idAttributeName];
+
+  var setPrefix = "set ";
+  var removePrefix = " remove ";
+
+  const attributes = Object.keys(item);
+  
+  if(attributes.length == 1) {
+    return;
+  }
+
+  var productImages;
+  var productImagesResized;
+  if (item.reorderImages) {
+    productImages = item.PRODUCT_IMAGES
+    productImagesResized = item.PRODUCT_IMAGES_RESIZED
+  } else if (item.PRODUCT_IMAGES) {
+    productImages = item.PRODUCT_IMAGES.map((image) => `https://${IMAGES_BUCKET}.s3.${REGION}.amazonaws.com/${item[idAttributeName]}/${encodeS3URI(image.name.replace(/ /g, ""))}`);
+    productImagesResized = item.PRODUCT_IMAGES.map((image) => `https://${IMAGES_BUCKET}.s3.${REGION}.amazonaws.com/${item[idAttributeName]}/resized-${encodeS3URI(image.name.replace(/ /g, ""))}`);
+  }
+
+  const removeAttributes = "removeAttributes"
+  const reorderImages = "reorderImages"
+
+  for (let i = 0; i < attributes.length; i++) {
+    const attribute = attributes[i];
+
+    if (attribute == removeAttributes || attribute == reorderImages) {
+      continue;
+    } else if (attribute != idAttributeName) {
+      params["UpdateExpression"] += setPrefix + "#" + attribute + " = :" + attribute;
+
+      if (attribute === "PRODUCT_IMAGES") {
+        params["ExpressionAttributeValues"][":" + attribute] = productImages;
+      } else if (attribute === "PRODUCT_IMAGES_RESIZED") {
+        params["ExpressionAttributeValues"][":" + attribute] = productImagesResized;
+      } else if (attribute === "PRODUCT_TAGS") {
+        params["ExpressionAttributeValues"][":" + attribute] = item[attribute].length > 0 ? docClient.createSet(item[attribute]) : docClient.createSet([NO_TAGS_STRING]);
+      } else {
+        params["ExpressionAttributeValues"][":" + attribute] = item[attribute]
+      }
+
+      params["ExpressionAttributeNames"]["#" + attribute] = attribute;
+      setPrefix = ", ";
+    }
+  }
+
+  if (item[removeAttributes]) {
+    item.removeAttributes.forEach((removeAttribute) => {
+      params["UpdateExpression"] += removePrefix + "#" + removeAttribute
+      params["ExpressionAttributeNames"]["#" + removeAttribute] = removeAttribute;
+      removePrefix = ", ";
+    })
+  }
+
+  await docClient.update(params).promise();
+}
+
+/*!
+ * node-s3-url-encode - Because s3 urls are annoying
+ */
+
+var encodings = {
+  '\+': "abc",
+  '\!': "bcd",
+  '\"': "cde",
+  '\#': "def",
+  '\$': "efg",
+  '\&': "fgh",
+  '\'': "ghi",
+  '\(': "hij",
+  '\)': "ijk",
+  '\*': "jkl",
+  '\,': "klm",
+  '\:': "lmn",
+  '\;': "mno",
+  '\=': "nop",
+  '\?': "opq",
+  '\@': "pqr",
+};
+
+function encodeS3URI(filename) {
+  return encodeURI(filename) // Do the standard url encoding
+              .replace(
+                  /(\+|!|"|#|\$|&|'|\(|\)|\*|\+|,|:|;|=|\?|@)/img,
+                  function(match) { return encodings[match]; }
+              );
+
+}
+
+async function emptyS3Directory(bucket, dir) {
+  const listParams = {
+      Bucket: bucket,
+      Prefix: dir
+  };
+
+  const listedObjects = await S3Client.listObjectsV2(listParams).promise();
+
+  if (listedObjects.Contents.length === 0) return;
+
+  const deleteParams = {
+      Bucket: bucket,
+      Delete: { Objects: [] }
+  };
+
+  listedObjects.Contents.forEach(({ Key }) => {
+      deleteParams.Delete.Objects.push({ Key });
+  });
+
+  await S3Client.deleteObjects(deleteParams).promise();
+
+  if (listedObjects.IsTruncated) await emptyS3Directory(bucket, dir);
+}
+
+const dealPriceIsHigherThanPrice = "O preço promocional deve ser menor do que o preço padrão do produto"
+
+const main = async (event) => {
+  const task = JSON.parse(event.body)
+  const id = task.id
+  const tags = task.PRODUCT_TAGS
+  const images = task.PRODUCT_IMAGES
+  const productOwnerId = event.requestContext.authorizer.id
+  const reorderImages = task.reorderImages
+  const dealPrice = task.DEAL_PRICE
+  const price = task.PRODUCT_PRICE
+
+  var response;
+  try {
+    response = await getItemFromDynamoDB(id)
+    console.log(`Item recuperado ${id} do dynamodb com sucesso`);
+  } catch (error) {
+    console.error(error)
+    return handleError({ message: "Erro ao recuperar item do dynamodb", statusCode: error.statusCode })
+  }
+
+  if (response.Item.PRODUCT_OWNER_ID !== productOwnerId) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ message: "Permissão de alterar o item negada." }),
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Content-Type": "application/json"
+      },
+      isBase64Encoded: false
+    };
+  }
+
+  if (dealPrice) {
+    if (price && dealPrice >= price) {
+      return handleError({ message: dealPriceIsHigherThanPrice, statusCode: 500 })
+    }
+
+    if (response.Item.PRODUCT_PRICE && dealPrice >= response.Item.PRODUCT_PRICE) {
+      return handleError({ message: dealPriceIsHigherThanPrice, statusCode: 500 })
+    }
+  }
+ 
+  if (reorderImages) {
+    const currentImagesOrder = response.Item.PRODUCT_IMAGES;
+    task.PRODUCT_IMAGES = reorderImages.map(i => currentImagesOrder[i]);
+
+    if (response.Item.PRODUCT_IMAGES_RESIZED) {
+      const currentResizedImagesOrder = response.Item.PRODUCT_IMAGES_RESIZED;
+      task.PRODUCT_IMAGES_RESIZED = reorderImages.map(i => currentResizedImagesOrder[i]);
+    }
+  }
+
+  const oldTags = response.Item?.PRODUCT_TAGS?.values
+
+  if (oldTags) {
+    for (const oldTag of oldTags) {
+      if (tags && tags.includes(oldTag)) {
+        continue;
+      }
+
+      try {
+        await removeProductFromTag(oldTag, id)
+      } catch (error) {
+        console.error(error)
+        return handleError({ message: "Erro ao remover produto de uma tag", statusCode: 400 })
+      }
+    }
+  }
+
+  if (tags) {
+    for (const tag of tags) {
+      if (oldTags && oldTags.includes(tag)) {
+        continue;
+      }
+  
+      try {
+        await addProductToTag(tag, id)
+      } catch (error) {
+        console.error(error)
+        return handleError({ message: "Erro ao adicionar produto a uma tag", statusCode: error.statusCode })
+      }
+    }
+  }
+
+  // if images is defined we remove all the old images
+  if (images) {
+    try {
+      await emptyS3Directory(IMAGES_BUCKET, `${id}/`);
+      console.log(`Item deletado ${id} do s3 bucket com sucesso`);
+    } catch (error) {
+      console.error(error)
+      return handleError({ message: "Erro ao esvaziar s3 bucket com as imagens de um produto", statusCode: error.statusCode });
+    }
+  }
+
+  // update parameters on dynamodb
+  try {
+    await updateItemOnDynamoDB(task, PRODUCTS_TABLE_PARTITION_KEY);
+    console.log("Item atualizado no dynamodb com sucesso");
+  } catch(error) {
+    console.error(error)
+    return handleError({ message: "Erro ao atualizar item no dynamodb", statusCode: error.statusCode });
+  }
+
+  // if images is defined, add the new images
+  if (images) {
+    var promises = [];
+
+    // add normal images
+    images.forEach((image) => {
+      const S3EncodedKey = `${id}/${encodeS3URI(image.name.replace(/ /g, ""))}`
+      const base64Data = new Buffer.from(image.content.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+      const type = image.content.split(';')[0].split('/')[1];
+    
+      const S3Params = {
+        Bucket: IMAGES_BUCKET,
+        Key: S3EncodedKey, // File name you want to save as in S3
+        Body: base64Data,
+        ContentEncoding: 'base64', // required
+        ContentType: `image/${type}` // required. Notice the back ticks
+      } 
+    
+      // Uploading files to the bucket
+      const promise = S3Client.upload(S3Params).promise();
+      promises.push(promise)
+    });
+
+    try {
+      await Promise.all(promises)
+      console.log("Imagens adicionadas ao s3 bucket com sucesso.")
+    } catch (error) {
+      console.error(error)
+      return handleError({ message: "Erro ao inserir imagem no s3 bucket", statusCode: 400 });
+    }
+
+    promises = []
+    // add resized images
+    images.forEach((image) => {
+      const S3EncodedKey = `${id}/resized-${encodeS3URI(image.name.replace(/ /g, ""))}`
+      const base64Data = new Buffer.from(image.contentResized.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+      const type = image.contentResized.split(';')[0].split('/')[1];
+    
+      const S3Params = {
+        Bucket: IMAGES_BUCKET,
+        Key: S3EncodedKey, // File name you want to save as in S3
+        Body: base64Data,
+        ContentEncoding: 'base64', // required
+        ContentType: `image/${type}` // required. Notice the back ticks
+      } 
+    
+      // Uploading files to the bucket
+      const promise = S3Client.upload(S3Params).promise();
+      promises.push(promise)
+    });
+
+    try {
+      await Promise.all(promises)
+      console.log("Imagens redimensionadas adicionadas ao s3 bucket com sucesso")
+    } catch (error) {
+      console.error(error)
+      return handleError({ message: "Erro ao inserir imagem redimensionada no s3 bucket", statusCode: 400 });
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Success"),
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Content-Type": "application/json"
+    },
+    isBase64Encoded: false
+  };
+}
+
+module.exports = { main }
