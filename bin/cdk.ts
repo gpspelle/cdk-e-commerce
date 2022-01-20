d17469aa6724da4efd9366daecf2444444ae5708
gpspelle <gpsunicamp016@gmail.com>
2022-01-20 15:13:29 -0300

    migrate to sa-east-1

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,9 +4,11 @@ import * as cdk from '@aws-cdk/core';
 import { ECommerceStack } from '../lib/e-commerce';
 
 const beta = { env: { account: '774004686218', region: 'us-east-1' }, sesEmailFrom: "araras.lojinha@gmail.com", imagesBucket: "beta-e-commerce-images-bucket" };
-const prod = { env: { account: '453299922179', region: 'us-east-1' }, sesEmailFrom: "gpsunicamp016@gmail.com", imagesBucket: "e-commerce-images-bucket", customDomain: "alojinha.click" };
+const USEastProd = { env: { account: '453299922179', region: 'us-east-1' }, sesEmailFrom: "gpsunicamp016@gmail.com", imagesBucket: "e-commerce-images-bucket" };
+const SAEastProd = { env: { account: '453299922179', region: 'sa-east-1' }, sesEmailFrom: "gpsunicamp016@gmail.com", imagesBucket: "sa-e-commerce-images-bucket", customDomain: "alojinha.click" };
 
 const app = new cdk.App();
 
 new ECommerceStack(app, 'ECommerceStackBeta', beta);
-new ECommerceStack(app, 'ECommerceStack', prod);
+new ECommerceStack(app, 'ECommerceStack', USEastProd);
+new ECommerceStack(app, 'EcommerceStackProdSAEast', SAEastProd);
