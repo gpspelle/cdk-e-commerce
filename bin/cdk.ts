4af72ddf5d591cf70032e121fa2f33d08bc5523b
gpspelle <gpsunicamp016@gmail.com>
2022-05-04 00:30:53 -0300

    include diagram and remove unused stack

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -10,13 +10,6 @@ const beta = {
     adminsBucket: "us-east-1-beta-e-commerce-admins-bucket"
 };
 
-const USEastProd = { 
-    env: { account: '453299922179', region: 'us-east-1' }, 
-    sesEmailFrom: "gpsunicamp016@gmail.com", 
-    imagesBucket: "e-commerce-images-bucket",
-    adminsBucket: "us-east-1-prod-e-commerce-admins-bucket"
-};
-
 const SAEastProd = { 
     env: { account: '453299922179', region: 'sa-east-1' }, 
     sesEmailFrom: "gpsunicamp016@gmail.com", 
@@ -36,5 +29,4 @@ const SAEastProd = {
 const app = new cdk.App();
 
 new ECommerceStack(app, 'ECommerceStackBeta', beta);
-new ECommerceStack(app, 'ECommerceStack', USEastProd);
 new ECommerceStack(app, 'EcommerceStackProdSAEast', SAEastProd);
